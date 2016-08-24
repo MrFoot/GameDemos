ebfde55b7b791008d93aac577fbad85b9f115a9d
MrFoot <linzhiwang7@gmail.com>
2016-08-24 08:36:07 +0800

    CharacterState

---
+++ Code/Assets/Scripts/GameState/CharacterState/CharacterStates.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterStates : BaseCharacterState {
+
+    public CharacterStates(CharacterStateManager mgr) : base(mgr)
+    { }
+}
